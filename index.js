c6c73f6f865d3f4ab64f1f32ced11278f77038b9
arb sht <shtjefni.arbi@iisgalvanimi.edu.it>
2024-03-20 23:04:27 +0000

    punto 2 e pagina di prova

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var app = express(); 
+const songs = require('./songs.json');
+app.set('view engine', 'pug');
+app.use(express.static(__dirname));
+
 app.get('/', function (req, res) {
-  res.send('Hello Wodfbrlsdfgsdfgdflkgjngb d!');
+    res.render('index', {
+        songs: songs.songs
+    });
+
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
